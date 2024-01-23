0491a0ea904eddfb30ef3cd5653d8267e912ded5
Kyle Kestell <kyle@kestell.org>
2024-01-23 15:01:33 -0600

    .

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -2,7 +2,7 @@
 
 public static class Program
 {
-    public static int Main()
+    public static async Task Main()
     {
         // var lexer = new Lexer(@"
         //     x = 1 + 2 * 3
@@ -24,60 +24,58 @@ public static class Program
         //         print(i)
         //     end for
         // ");
-//          var lexer = new Lexer(@"
-//          # arithmetic
-//
-//          x = 1 + 2 - 3 * 4 / 5
-//          y = 1 + (2 - (3 * (4 / 5)))
-//
-//          # logic
-//
-//          if x > 1 then
-//              print(""blah"")
-//          else if x < 0.5 then
-//              print(""whoa"")
-//          else
-//              print(""meh"")
-//          end if
-//
-//          # loops
-//
-//          for i = 1 to 5
-//              print(i)
-//              if i >= 2 then
-//                  break
-//              end if
-//          end for
-//
-//          people = [""Alice"", ""Bob"", ""Carol""]
-//          for person in people
-//              print(person)
-//          end for
-//
-//          j = 0
-//          while j <= 10
-//              j = j + 1
-//          end while
-//
-//          k = 0
-//          while true
-//              k = k + 1
-//              if k >= 100 then
-//                  break
-//              end if
-//          end while
-//          ");
-        // var parser = new Parser(lexer);
-        // var interpreter = new Interpreter();
-        // while (true)
-        // {
-        //     var statement = parser.ParseStatement();
-        //     
-        //     if (statement is null)
-        //         break;
-        //
-        //     interpreter.ExecuteStatement(statement);
-        // }
-        return 0;
+        var lexer = new Lexer("""
+          # arithmetic
+
+          x = 1 + 2 - 3 * 4 / 5
+          y = 1 + (2 - (3 * (4 / 5)))
+          print(x)
+          print(y)
+
+          # logic
+
+          IF x > 1 THEN
+              print("blah")
+          ELSE IF x < 0.5 THEN
+              print("whoa")
+          ELSE
+              print("meh")
+          END IF
+
+          FOR i = 1 TO 5 THEN
+              print(i)
+          END FOR
+
+          x = 0
+          WHILE x < 10 THEN
+              x = x + 1
+              print(x)
+          END WHILE
+
+          name = "Alice"
+          print("Hello, {name}!")
+          
+          x = 10
+          WHILE TRUE THEN
+              print(x)
+              x = x - 1
+              IF x < 0 THEN
+                  BREAK
+              END IF
+          END WHILE
+
+        """);
+        var parser = new Parser(lexer);
+        var interpreter = new Interpreter();
+        while (true)
+        {
+            var statement = parser.ParseStatement();
+
+            if (statement is null)
+                break;
+
+            await interpreter.ExecuteStatement(statement);
+        }
+        return;
     }
 }
